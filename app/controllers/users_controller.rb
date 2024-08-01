30b3f7dbf54dfbdcf92ab876946e077dc8fd155e
hahung200903 <hangochung.edu@gmail.com>
2024-08-01 22:50:16 +0700

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    redirect_to root_url and return unless FILL_IN
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -49,31 +49,21 @@ def destroy
   end
 
   private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password,
-                                   :password_confirmation)
-    end
+  # Before filters
 
-    # Before filters
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url, status: :see_other) unless current_user?(@user)
+  end
 
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url, status: :see_other
-      end
-    end
-
-    # Confirms the correct user.
-    def correct_user
-      @user = User.find(params[:id])
-      redirect_to(root_url, status: :see_other) unless current_user?(@user)
-    end
-
-    # Confirms an admin user.
-    def admin_user
-      redirect_to(root_url, status: :see_other) unless current_user.admin?
-    end
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url, status: :see_other) unless current_user.admin?
+  end
 end
--- test/integration/password_resets_test.rb
+++ test/integration/password_resets_test.rb
@@ -97,6 +97,24 @@ class PasswordUpdateTest < PasswordResetForm
   end
 end
 
+class ExpiredToken < PasswordResets
+
+  def setup
+    super
+    # Create a password-reset token.
+    post password_resets_path,
+         params: { password_reset: { email: @user.email } }
+    @reset_user = assigns(:user)
+    # Expire the token by hand.
+    @reset_user.update_attribute(:reset_sent_at, 3.hours.ago)
+    # Attempt to update the user's password.
+    patch password_reset_path(@reset_user.reset_token),
+          params: { email: @reset_user.email,
+                    user: { password:              "foobar",
+                            password_confirmation: "foobar" } }
+  end
+end
+
 class ExpiredTokenTest < ExpiredToken
 
   test "should redirect to the password-reset page" do
